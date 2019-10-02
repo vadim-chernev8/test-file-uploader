c35ddad225af28e452e64ceb5a17d7d3cdf6a07e
Vadim <vadimchernev16@gmail.com>
2019-10-02 12:30:22 +0300

    Updates

---
+++ precache-manifest.2943fc7ebdbcdb5362385e7a5f56ef0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c04c2eb914d117aaf74598cdcedc3b4",
+    "url": "/vadim-chernev8/test-file-uploader/index.html"
+  },
+  {
+    "revision": "98186aa6e3cd8d9feae1",
+    "url": "/vadim-chernev8/test-file-uploader/static/css/main.12b7ef19.chunk.css"
+  },
+  {
+    "revision": "ca5ae286f2040e56f312",
+    "url": "/vadim-chernev8/test-file-uploader/static/js/2.bb351494.chunk.js"
+  },
+  {
+    "revision": "98186aa6e3cd8d9feae1",
+    "url": "/vadim-chernev8/test-file-uploader/static/js/main.fcbd79c2.chunk.js"
+  },
+  {
+    "revision": "7135879ef73e79bcef1e",
+    "url": "/vadim-chernev8/test-file-uploader/static/js/runtime-main.d68dee7b.js"
+  }
+]);
